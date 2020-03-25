1c91d665a1ef2695647431c048b9f6e4caa5a324
= <=>
2020-03-25 12:51:30 -0300

    Added a crypted password

--- database/inserts.sql
+++ database/inserts.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (name, username, password) VALUES 
-    ('User Test', 'user', 'pass123');
+    ('User Test', 'user', '32250170a0dca92d53ec9624f336ca24');
 
 INSERT INTO settings (half_price, whole_price) VALUES 
     (DEFAULT, DEFAULT);
